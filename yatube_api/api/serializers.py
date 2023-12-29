0eadec40802cb196191b1b4809283a7f72362c25
Artem Minnigaliev <artyomminnigaliev@gmail.com>
2023-12-28 22:34:38 -0800

    Work complete

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -1,16 +1,11 @@
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
+from rest_framework.validators import UniqueTogetherValidator
 
+from posts.models import Comment, Follow, Group, Post
 
-from posts.models import Comment, Post
-
-
-class PostSerializer(serializers.ModelSerializer):
-    author = SlugRelatedField(slug_field='username', read_only=True)
-
-    class Meta:
-        fields = '__all__'
-        model = Post
+User = get_user_model()
 
 
 class CommentSerializer(serializers.ModelSerializer):
@@ -19,5 +14,47 @@ class CommentSerializer(serializers.ModelSerializer):
     )
 
     class Meta:
-        fields = '__all__'
         model = Comment
+        fields = '__all__'
+        read_only_fields = ('post',)
+
+
+class GroupSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Group
+        fields = '__all__'
+
+
+class PostSerializer(serializers.ModelSerializer):
+    author = SlugRelatedField(slug_field='username', read_only=True)
+
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+
+class FollowSerializer(serializers.ModelSerializer):
+    user = serializers.SlugRelatedField(
+        read_only=True,
+        default=serializers.CurrentUserDefault(),
+        slug_field='username'
+    )
+    following = serializers.SlugRelatedField(
+        queryset=User.objects.all(), slug_field='username')
+
+    class Meta:
+        model = Follow
+        fields = '__all__'
+
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Follow.objects.all(),
+                fields=['user', 'following']
+            )
+        ]
+
+    def create(self, validated_data):
+        if validated_data['user'] == validated_data['following']:
+            raise serializers.ValidationError()
+        return super().create(validated_data)
--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,19 @@
-from django.urls import path
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
+
+app_name = 'api'
+
+router = DefaultRouter()
+router.register(
+    r'posts/(?P<post_id>\d+)/comments', CommentViewSet, basename='comment')
+router.register('follow', FollowViewSet, basename='follow')
+router.register('groups', GroupViewSet, basename='group')
+router.register('posts', PostViewSet, basename='post')
 
 urlpatterns = [
+    path('v1/', include(router.urls)),
+    path('v1/auth/', include('djoser.urls')),
+    path('v1/', include('djoser.urls.jwt')),
 ]
